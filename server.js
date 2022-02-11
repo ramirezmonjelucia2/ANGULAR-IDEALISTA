d531c9a08be720ea328e25fb9154026cc8abeff1
Ramirez Monje Lucia <lrammon392@g.educaand.es>
2022-02-11 09:37:52 +0100

    Version three

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-app.use(express.static('./build/'));
+app.use(express.static('./build/a-idealista'));
 app.get('/*', function(req, res) {
- res.sendFile('index.html', {root: '/build/'}
+ res.sendFile('index.html', {root: '/build/a-idealista/'}
  );
 });
 const port = 3500;
