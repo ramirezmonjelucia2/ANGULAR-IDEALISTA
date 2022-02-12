396357d654bcf9e1c7f1b7bf103a7a7c06a2553e
Ramirez Monje Lucia <lrammon392@g.educaand.es>
2022-02-12 17:10:55 +0100

    Version six

--- src/app/shared/services/empleado.service.ts
+++ src/app/shared/services/empleado.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class EmpleadoService {
-  private url = "https://idealistav21.herokuapp.com/"
+  private url = "https://idealista-api.herokuapp.com/"
 
   constructor(private httpClient: HttpClient) {
   }
